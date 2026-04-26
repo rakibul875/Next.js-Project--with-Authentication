81bdb94a178235d8a238080dbeeaecdc801b0588
rakibul875 <rakibulislamashik78@gmail.com>
2026-04-26 18:46:47 +0600

    Add About and Career pages; enhance NavBar with navigation links and user login button

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,9 +1,22 @@
+import Link from 'next/link';
 import React from 'react';
+import NavLink from './NavLink';
+import Image from 'next/image';
+import userAvatar from '@/assets/image/user.png'
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>NavBar</h1>
+        <div className='flex justify-between container my-5 mx-auto'>
+            <div></div>
+            <ul className='flex justify-between gap-3'>
+                <li><NavLink href={'/'}>Home</NavLink></li>
+                <li><NavLink href={'/about-us'}>About</NavLink></li>
+                <li><NavLink href={'/career'}>Career</NavLink></li>
+            </ul>
+            <div className="flex items-center gap-2">
+                <Image src={userAvatar} width={50} height={50} alt='auth'/>
+                <button className='btn bg-purple-400'>Login</button>
+            </div>
         </div>
     );
 };
