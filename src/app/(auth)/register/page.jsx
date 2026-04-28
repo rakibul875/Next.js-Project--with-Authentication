d1a04a3e8a65ff0840425c4c2a7852176c864630
rakibul875 <rakibulislamashik78@gmail.com>
2026-04-28 12:42:10 +0600

    - Created auth.js for better-auth configuration with MongoDB. - Added route.js for API handling of authentication requests. - Enhanced register page to handle user registration with error handling.

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,14 +1,33 @@
-"use client"
+"use client";
 
+import { authClient } from "@/lib/auth-client";
 import React from "react";
 import { useForm } from "react-hook-form";
 
 const registerPage = () => {
-      const { register, handleSubmit, formState: { errors } } = useForm();
-    
-      const handelRegister= (data)=>{
-        console.log(data)
-      }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handelRegister = async (data) => {
+    const { name, email, password, photo } = data;
+
+    const {data:res, error} = await authClient.signUp.email({
+      name: name, 
+      email:email, 
+      password: password,
+      callbackURL: '/',
+    });
+    console.log(res,error)
+    if(error){
+      alert(error.message)
+    }
+    if(res){
+      alert('SinUP successfully')
+    }
+  };
   return (
     <div className="flex justify-center items-center bg-slate-100 container mx-auto mt-20 rounded-2xl">
       <div className="bg-white p-10 rounded-2xl my-5">
@@ -21,9 +40,11 @@ const registerPage = () => {
               type="text"
               className="input"
               placeholder="Enter your name"
-               {...register("name",{ required: 'Name is required' })}
+              {...register("name", { required: "Name is required" })}
             />
-            {errors.password&&<p className="text-red-600">{errors.password.message}</p>}
+            {errors.password && (
+              <p className="text-red-600">{errors.password.message}</p>
+            )}
           </fieldset>
           <fieldset className="fieldset">
             <legend className="fieldset-legend text-xl">Photo URL</legend>
@@ -31,9 +52,11 @@ const registerPage = () => {
               type="text"
               className="input"
               placeholder="Enter your Photo URL"
-               {...register("photo",{ required: 'Photo URL is required' })}
+              {...register("photo", { required: "Photo URL is required" })}
             />
-            {errors.password&&<p className="text-red-600">{errors.password.message}</p>}
+            {errors.password && (
+              <p className="text-red-600">{errors.password.message}</p>
+            )}
           </fieldset>
           <fieldset className="fieldset">
             <legend className="fieldset-legend text-xl">Email Address</legend>
@@ -41,9 +64,11 @@ const registerPage = () => {
               type="email"
               className="input"
               placeholder="Enter your Email"
-               {...register("email",{ required: 'Email is required' })}
+              {...register("email", { required: "Email is required" })}
             />
-            {errors.password&&<p className="text-red-600">{errors.password.message}</p>}
+            {errors.password && (
+              <p className="text-red-600">{errors.password.message}</p>
+            )}
           </fieldset>
           <fieldset className="fieldset">
             <legend className="fieldset-legend text-xl">Password</legend>
@@ -51,9 +76,11 @@ const registerPage = () => {
               type="password"
               className="input"
               placeholder="Enter your Password"
-               {...register("password",{ required: 'Password is required' })}
+              {...register("password", { required: "Password is required" })}
             />
-            {errors.password&&<p className="text-red-600">{errors.password.message}</p>}
+            {errors.password && (
+              <p className="text-red-600">{errors.password.message}</p>
+            )}
           </fieldset>
           <button className="btn w-full bg-slate-800 text-white">Login</button>
         </form>
